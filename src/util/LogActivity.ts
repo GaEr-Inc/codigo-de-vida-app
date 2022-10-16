1e2bb64f2ecc067691fe17ebb9338ea3ba62e0a2
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-10-16 11:21:41 -0500

    add: local builds

--- src/util/LogActivity.ts
+++ src/util/LogActivity.ts
@@ -4,9 +4,7 @@ import { createClient } from "./PocketbaseUtil";
 export async function logAccess(id: string, cedula:string, from: string) {
   const client = await createClient(SERVER_URL);
   client.records.create("registros", {
-  "id_usuario": client.authStore.model?.id,
   "email_usuario": client.authStore.model?.email,
-  "id_paciente": id,
   "cedula_paciente": cedula,
   "desde": from,
   "cliente": "app"
